709b2a0c6b1860f9b1584286f4a5ce7aebc5f8a8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-02-14 00:28:51 +0100

    :fire: Removed personal fonts

--- install-fonts.sh
+++ install-fonts.sh
@@ -8,10 +8,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 brew cask install homebrew/cask-fonts/font-open-sans
 brew cask install homebrew/cask-fonts/font-roboto
 brew cask install homebrew/cask-fonts/font-hack
-brew cask install homebrew/cask-fonts/font-impact
 brew cask install homebrew/cask-fonts/font-public-sans
-brew cask install homebrew/cask-fonts/font-raleway
-brew cask install homebrew/cask-fonts/font-oswald
 
 # Cleanup
 brew cleanup
